fcf90974f3b467396e1a82e0156401cc820cee3c
mbattur <mbattur@yahoo.com>
2016-01-12 20:31:37 +0000

    assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -2,9 +2,21 @@
  
  class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
+    end
+    
+    def remove_entry(name, phone, email)
+        delete_entry = nil
+        
+        @entries.each do |entry|
+            if name == entry.name && phone == entry.phone_number && email == entry.email
+                delete_entry = entry
+            end
+        end
+        
+        @entries.delete(delete_entry)
     end
     
     def add_entry(name, phone_number, email)
